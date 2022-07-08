c36a2d3f7b6f36430ce5d6f6e3f69111dc1c3317
sasha-kharag <sasha@weimark.com>
2022-07-08 18:32:48 +0300

    updates to ftyles

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -2,42 +2,43 @@ import { useState } from 'react';
 import './Filter.css';
 
 export type FilterProps = {
-    id: string;
-    name?: string;
-    src?: string;
-    alt?: string;
-    SelectFilter?: () => void;
-}
+	id: string;
+	name?: string;
+	src?: string;
+	alt?: string;
+	SelectFilter?: () => void;
+};
 
 const Fitler = ({ id, name, src, alt, SelectFilter }: FilterProps) => {
-    const [selected, setSelected] = useState(false);
+	const [selected, setSelected] = useState(false);
 
-    const ClickHandler = () => {
-        setSelected(!selected)
-        if (SelectFilter) SelectFilter();
-    }
+	const ClickHandler = () => {
+		setSelected(!selected);
+		if (SelectFilter) SelectFilter();
+	};
 
-    const CapitalFirstLetter = (str: string): string => {
-        const arr = str.split(" ");
+	const CapitalFirstLetter = (str: string): string => {
+		const arr = str.split(' ');
 
-        for (var i = 0; i < arr.length; i++)
-            arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+		for (var i = 0; i < arr.length; i++)
+			arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
 
-        const new_str = arr.join(" ");
+		const new_str = arr.join(' ');
 
-        return new_str;
-    }
+		return new_str;
+	};
 
-    return (
-
-        <button
-            id={id}
-            className={`filter${selected ? ' selected' : ''}`}
-            onClick={ClickHandler}>
-            {src && <img src={src} alt={alt || `${(name || id).toLowerCase()} thumbnail`} />}
-            <h3>{CapitalFirstLetter(name || id)}</h3>
-        </button>
-    );
-}
+	return (
+		<button
+			id={id}
+			className={`filter${selected ? ' selected' : ''}`}
+			onClick={ClickHandler}>
+			{src && (
+				<img src={src} alt={alt || `${(name || id).toLowerCase()} thumbnail`} />
+			)}
+			<h3>{CapitalFirstLetter(name || id)}</h3>
+		</button>
+	);
+};
 
 export default Fitler;
