676e32d392943ef2a8c157fba9ebe6f19c6a5908
sasha-kharag <sasha@weimark.com>
2023-02-05 16:00:06 +0200

    added memoizing of list

--- src/components/DrinksList/DrinksList.tsx
+++ src/components/DrinksList/DrinksList.tsx
@@ -1,12 +1,9 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useMemo } from 'react';
 import { DrinkListContext } from 'hooks/contexts/DrinkListContext';
 import Drink from '../Drink/Drink';
 import './DrinkList.css';
 import { DrinkType } from 'utils/types';
-import {
-	FilterListContext,
-	FilterListProvider
-} from 'hooks/contexts/FiltersContext';
+import { FilterListContext } from 'hooks/contexts/FiltersContext';
 import { Map } from 'typescript';
 
 //TODO Move CSS Styles from App.css to DrinkList.css
@@ -14,14 +11,38 @@ import { Map } from 'typescript';
 const DrinksList = () => {
 	const { drinkList } = useContext(DrinkListContext);
 	const { filters } = useContext(FilterListContext);
-	const showList = drinkList;
-	const removedList = new Map<string, DrinkType[]>();
-
-	useEffect(() => {
-		for (const entry in filters.entries) {
-			console.log('entry', entry);
+	// const showList = drinkList;
+	const showList = useMemo(() => {
+		let list = drinkList;
+		for (const [filterType, filterArr] of filters) {
+			filterArr.forEach((filterId) => {
+				list.filter((drink) => {
+					console.log(drink, filterType, drink[filterType], filterId);
+					return drink[filterType] === filterId;
+				});
+			});
+			console.log(list);
 		}
-	}, [filters]);
+		// for (const key of filters.keys()) {
+		// 	console.log(key, filters.get(key));
+		// 	for (const value in filters.get(key)) {
+		// 		console.log(value);
+		// 		list = list.filter((drink) => {
+		// 			console.log(drink);
+		// 			return drink[key] === value;
+		// 		});
+		// 	}
+		// }
+		// console.log(list);
+		return [...list];
+	}, [filters, drinkList]);
+
+	// useEffect(() => console.log(showList), [showList]);
+
+	// useEffect(() => {
+	// 	for (const k in filters.keys()) console.log(k);
+	// 	for (const v in filters.values()) console.log('value', v);
+	// }, [filters, filters.keys, filters.values]);
 
 	return (
 		<div id='list'>
